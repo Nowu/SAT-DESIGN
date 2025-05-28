ddc74b70a7a9e07395e5555f97d408b876ca9512
FRA0020 <FRA0020@T5-MSI-PC-09>
2025-05-29 09:48:26 +1000

    Add project files.

---
+++ WindowsApp1/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WindowsApp1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Department of Education & Training, Victoria")>
+<Assembly: AssemblyProduct("WindowsApp1")>
+<Assembly: AssemblyCopyright("Copyright © Department of Education & Training, Victoria 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("81501c8a-bdf5-4f2d-98c2-2c3cbb314d6d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
